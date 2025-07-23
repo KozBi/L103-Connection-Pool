edb7d3c6a540691ad8095e09a0d782d0deb1d4a0
KozBi <kozlo7830@gmail.com>
2025-07-23 19:54:35 +0200

    sqlite

--- my_classes/my_code.py
+++ my_classes/my_code.py
@@ -1,50 +1,29 @@
 import sqlite3
 from dbConnectionPool import ConnectionPool
 from dbConnectionPool import PooledConnection
+import hashlib
 
 
-cp=ConnectionPool()
-project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30')
+cp=ConnectionPool(database='mailbox')
 
-def add_project(conn:PooledConnection, project):
-    # insert table statement
-    sql = ''' INSERT INTO projects(name,begin_date,end_date)
-              VALUES(?,?,?) '''
-    
-    # Create  a cursor
-    cur = conn.cursor()
-
-    # execute the INSERT statement
-    cur.execute(sql, project)
-    
-    # commit the changes
-    conn.commit()
-
-    # get the id of the last inserted row
-    return cur.lastrowid
 
 try:
     conn=cp.get_connection()
 
     curr=conn.cursor()
-    task1="""CREATE TABLE IF NOT EXISTS projects (
-        id INTEGER PRIMARY KEY, 
-        name text NOT NULL, 
-        begin_date DATE, 
-        end_date DATE
-    );"""
-    curr.execute(task1)
+    task1="""INSERT INTO users(username, password_hash,is_admin) VALUES(?,?,?)"""
+    login="admin"
+    password='admin'
+    password=str(hashlib.sha256(password.encode()).hexdigest())
+    values=(login,password,True)
+    print(values)
+    curr.execute(task1,values)
     conn.commit()
-   # project_id = add_project(conn, project)
-  #  print(f'Created a project with the id {project_id}')
 
 except sqlite3.OperationalError as e:
     print("Failed to create tables:", e)
 
 con1=cp.get_connection()
 cur1=con1.cursor()
-sql_delete = 'DELETE FROM projects WHERE id = ?'
-cur1.execute(sql_delete, (2,))
-cur1.execute(sql_delete, (3,))
-cur1.execute("SELECT * FROM  projects")
+cur1.execute("SELECT * FROM users")
 print(cur1.fetchall())
